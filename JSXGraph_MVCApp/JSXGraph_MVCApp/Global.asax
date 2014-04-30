b414e59340d73f57edef9e84390e892376e8be86
unknown <pastor.cortes@WSOS001.Ahinko.com>
2014-04-30 17:58:12 -0600

    Primer commit para proyecto de prueba

---
+++ JSXGraph_MVCApp/JSXGraph_MVCApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSXGraph_MVCApp.MvcApplication" Language="C#" %>
---
+++ JSXGraph_MVCApp/JSXGraph_MVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JSXGraph_MVCApp.Startup))]
+namespace JSXGraph_MVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
