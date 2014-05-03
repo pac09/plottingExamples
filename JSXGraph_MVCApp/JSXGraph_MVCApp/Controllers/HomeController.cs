eb5475ab43b324f67e39922eaaa4dafc0f4f5e3e
unknown <pastor.cortes@WSOS001.Ahinko.com>
2014-05-02 18:19:17 -0600

    Addition of prototype and JSXGraph and implementation of code in a HomeController method

--- JSXGraph_MVCApp/JSXGraph_MVCApp/Controllers/HomeController.cs
+++ JSXGraph_MVCApp/JSXGraph_MVCApp/Controllers/HomeController.cs
@@ -24,7 +24,37 @@ namespace JSXGraph_MVCApp.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            string[] t = new string[0];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 70; i++)
+            {
+                Int32[] x = new Int32[0];
+                x[i] = i;
+
+                Int32[] y = new Int32[0];
+                y[i] = rnd.Next(0, 20);
+
+                t[i] = x[i] + "," + y[i] + ";";
+                //$t = "";
+                //for ($i=0;$i<70;$i++) {
+                //    $x = $i;
+                //    $y = mt_rand(0,20);
+                //    $t .= $x.','.$y.';';
+                //}
+                //echo $t;
+
+            }
+
+            ViewData["t"] = t;
+            //<% var myArray = (string[])ViewData["passedArray"]; %>
             return View();
         }
+
+
+        //[HttpPost]
+        //public ActionResult Contact() 
+        //{
+        //}
     }
 }
